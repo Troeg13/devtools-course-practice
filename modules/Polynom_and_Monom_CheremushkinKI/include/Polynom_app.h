391ecdd8ba0b8062fb40168d884d71f19269c0c9
Александра Троегубова <troeg1306@gmail.com>
2022-05-04 08:00:23 +0300

    fix

--- modules/Polynom_and_Monom_CheremushkinKI/include/Polynom_app.h
+++ modules/Polynom_and_Monom_CheremushkinKI/include/Polynom_app.h
@@ -8,11 +8,11 @@
 #include <string>
 
 class PolynomCalculator {
-public:
+ public:
     PolynomCalculator();
     std::string operator()(int argc, const char** argv);
 
-private:
+ private:
     void help(const char* appname, const char* message = "");
     bool validateNumberOfArguments(int argc, const char** argv);
     std::string message_;
